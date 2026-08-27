1f74b6a199618d7462042c2e047988e878404690
beaver <meyar2223@gmail.com>
2026-08-27 16:09:17 +0300

    add other layers to the parallax bg, add signals when the door access was changed

--- scripts/compartment.gd
+++ scripts/compartment.gd
@@ -2,9 +2,19 @@ extends Node
 
 @onready var spawn_marker: Marker2D = $SpawnMarker
 @onready var player = get_tree().get_first_node_in_group("player")
+@onready var interactable: Interactable = %Interactable
+
+func _ready() -> void:
+	GameEvents.door_access_changed.connect(_on_door_access_changed)
 
 func move_player_to_spawn() -> void:
 	player.global_position = spawn_marker.global_position
 
 func peep_through_peephole() -> void:
 	TransitionScene.transition_to("")
+
+func _on_door_access_changed(is_acessible: bool) -> void:
+	if is_acessible:
+		interactable.monitoring = true
+	else:
+		interactable.monitoring = false
--- scripts/game_events.gd
+++ scripts/game_events.gd
@@ -1,6 +1,7 @@
 extends Node
 
 signal train_state_changed(state)
+signal door_access_changed(is_accesible: bool)
 
 enum TrainState {
 	Corridor,
